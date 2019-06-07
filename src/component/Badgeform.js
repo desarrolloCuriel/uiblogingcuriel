af0166a6f1da0fa06ad8c4d07b3654a6e3a40e5d
eleazar-00@hotmail.com <eleazar.curiel@famsa.com>
2019-06-07 11:26:36 -0500

    Manejo de state

--- src/component/Badgeform.js
+++ src/component/Badgeform.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 class Badgeform extends React.Component{
+  state={};
 
   handleChage=e=>{
-      console.log({name:e.target.name,
-                   value:e.target.value});
+   //   console.log({name:e.target.name,
+   //                value:e.target.value});
+   this.setState(
+     {
+       [e.target.name]:e.target.value,
+     }
+   )
+
   }
   handleClick=e=>{
     console.log('Algo sucedio');
@@ -12,6 +19,7 @@ class Badgeform extends React.Component{
     handleOnsubmit=e=>{
         e.preventDefault();
         console.log("evento onSubmit")
+        console.log(this.state)
     }
   render(){
       return (
@@ -20,7 +28,23 @@ class Badgeform extends React.Component{
              <form onSubmit={this.handleOnsubmit}>
                <div className="form-group">
                    <label>First Name</label>
-                   <input onChange={this.handleChage} className="form-control" type="text" name="firstname"/>
+                   <input onChange={this.handleChage} className="form-control" type="text" name="firstname" value={this.state.firstname}/>
+               </div>
+               <div className="form-group">
+                   <label>Last Name</label>
+                   <input onChange={this.handleChage} className="form-control" type="text" name="lastname" value={this.state.lastname}/>
+               </div>
+               <div className="form-group">
+                   <label>Email</label>
+                   <input onChange={this.handleChage} className="form-control" type="email" name="email" value={this.state.email}/>
+               </div>
+               <div className="form-group">
+                   <label>Job Title</label>
+                   <input onChange={this.handleChage} className="form-control" type="text" name="jobtitle" value={this.state.jobtitle}/>
+               </div>
+               <div className="form-group">
+                   <label>twitter</label>
+                   <input onChange={this.handleChage} className="form-control" type="text" name="twitter" value={this.state.twitter}/>
                </div>
                <button onClick={this.handleClick} className="btn btn-primary">Save</button>
              </form>
