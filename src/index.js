ceef342d1d9d3efdce30646f2c63d37f955e40be
Eleazar Curiel Monjaraz <eleazar-00@hotmail.com>
2020-02-07 10:09:18 -0600

    se remonbra el proyecto

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BadgeNew from './pages/BadgeNew'
-import Badges from './pages/Badges'
-import App from './component/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import BadgeNew from "./pages/BadgeNew";
+import Badges from "./pages/Badges";
+import App from "./component/App";
 //importar bootstraps
-import 'bootstrap/dist/css/bootstrap.css'
-import './styles/global.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles/global.css";
 
-const container =document.getElementById('app')
+const container = document.getElementById("app");
 
 //reactDOM recibe dos elementos (--que-- , donde)
-ReactDOM.render(<App/>, container);
+ReactDOM.render(<App />, container);
